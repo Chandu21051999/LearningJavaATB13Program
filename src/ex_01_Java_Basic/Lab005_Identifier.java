7aa1dbda97c75c25cafe47e0efd5901a62b86f14
Chandu21051999 <158189479+Chandu21051999@users.noreply.github.com>
2025-06-09 21:23:47 +0530

    Commit and push

---
+++ src/ex_01_Java_Basic/Lab005_Identifier.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Identifier {
+    public static void main(String[] args) {
+
+        System.out.println("Hi +hello+Welcone");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab006_Identifiers.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Identifiers {
+
+    public static void main(String[] args) {
+        System.out.println("How main keywords, Identifier");
+        // 5 keywords and 3 Identifiers
+    }
+}
